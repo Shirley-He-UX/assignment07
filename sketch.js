b92f83484adf9fe9554fbda38e009954b2a62f41
Hadesnow <snowbloom1991@gmail.com>
2017-11-18 18:00:17 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,42 @@
+var capture;
 var mic;
-var c=1;
-
-function setup(){
-    createCanvas(500,500);
-    mic = new p5.AudioIn();
-//    recorder = new p5.SoundRecorder();
-
-    mic.start();
-    
-//    recorder.setInput(mic);
+function setup() {
+  createCanvas(500,500);
+  
+  mic = new p5.AudioIn();
+  capture = createCapture(VIDEO);
+  capture.size(500,500);
+  
+  capture.hide();
+  
+  mic.start();
+  angleMode(DEGREES);
   background(0);
+
 }
 
 function draw() {
-  background(0);
-  var vol = mic.getLevel();
-  var h=map(vol,0,1,0,height);
-    if(mouseIsPressed==true){
-        c++;
-    }
-      console.log(vol);
-   for(var x=0; x<width;x+=width/c){
-       for(var y = 0;y<height;y+=random(height/h)){
-           for(var count=0; count<=height/h;count++){
-               if(count%2==0){fill(255);}else{fill(0);}
-           }
-           rect(x,y,width/c,random(height/vol));
-         }     
-   }
+  
+  var myImg=capture.loadPixels();
+  var vol=mic.getLevel();
+  var h =map(vol,0,1,0,height*2);
+  translate(width/2,height/2);
+  push();
+  for(var i=0;i<=500;i+=30){
+    rotate(30);
+    //tint(255,100);
+    tint(50,50,205,200);
+    image(myImg, 0, 0, 500-i, 500-i);
+  }
+  pop();
+  
+  for(var i=-250;i<251;i+=20){
+         noStroke();
+          fill(255);
+          ellipse(sin(frameCount+i)*h,i,6);
+          ellipse(cos(frameCount+i)*h,i,6);
+          strokeWeight(1);
+          stroke(255);
+          line(sin(frameCount+i)*h,i,cos(frameCount+i)*h,i);
+     }
 }
